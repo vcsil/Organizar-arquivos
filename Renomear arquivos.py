03c5ffe9d7f7db2a4e0ab233edb382912d93729e
vcsil <vcsilva@poli.ufrj.br>
2022-12-25 19:28:14 -0300

    :sparkles: feat: controls the extension and refactor the rename function

--- Renomear arquivos.py
+++ Renomear arquivos.py
@@ -9,50 +9,56 @@ import os
 from tqdm import tqdm
 
 path_user = str(input("Current path: ")).replace('\\', '/')
-ext_user = str(input("New extension [.xxx]: "))
+extension_file_user = str(input("New extension [.xxx]: "))
+
+try:
+    use_old_name = bool(int(input("Use old name (0) or just number (1)?: ")))
+except ValueError:
+    use_old_name = bool(int(input("Use old name (0) or just number (1)?: ")))
+
+
+def get_file_extension(filename):
+    filename_reverse = filename[::-1]
+    dot_position = filename_reverse.index('.')
+    ext = filename_reverse[:dot_position][::-1]
+    return '.' + ext
 
 def get_first_extension(directory):
-    first_arq = os.listdir(directory)[0][::-1]   # Pega o nome do arquivo invertido para pegar o último '.''
-    dot_position = first_arq.index('.')
-    ext = first_arq[:dot_position][::-1]
-    return '.' + ext
+    first_arq = os.listdir(directory)[0]
+    return get_file_extension(first_arq)
+
 
 pos_initial = 1
 path = path_user if path_user != '' else "./"
-arq = ext_user if ext_user != '' else get_first_extension(path)
 
-l = os.listdir(path)
+filename_list = os.listdir(path)
 
-for nome in l:
-    novo_nome = str(pos_initial) + arq
+def rename_file(filename_list, pos_initial, ext_user):
+    novo_nome = str(pos_initial) + ext_user
     
     try:
-            #os.rename(path + '/' + nome, path + '/' + novo_nome )
+            #os.rename(path + '/' + filename, path + '/' + novo_nome )
             pos_initial += 1
     except:
-        while novo_nome in l:
-            indice = l.index(novo_nome)
-            l.pop(indice)
+        while novo_nome in filename_list:
+            indice = filename_list.index(novo_nome)
+            filename_list.pop(indice)
             
             pos_initial += 1
-            novo_nome = str(pos_initial) + arq
+            novo_nome = str(pos_initial) + ext_user
 
-        #os.rename(path + '/' + nome, path + '/' + novo_nome )
+        #os.rename(path + '/' + filename, path + '/' + novo_nome )
 
 
-    #print("arquivo " + nome + " alterado para " + novo_nome)
-    print(nome)
+    #print("arquivo " + filename + " alterado para " + novo_nome)
+    print(filename)
     print(novo_nome)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    exit()
+
+if extension_file_user == "":
+    for filename in tqdm(filename_list):
+        extension_file_user = get_file_extension(filename)
+        rename_file(filename_list, 1, extension_file_user)
+else:
+    for filename in tqdm(filename_list):
+        rename_file(filename_list, 1, extension_file_user)
