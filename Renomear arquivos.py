734f823b2d0ab072599f79c5f774acdc79e085e6
vcsil <vcsilva@poli.ufrj.br>
2022-12-26 01:00:01 -0300

    :sparkles: feat: controls count digits

--- Renomear arquivos.py
+++ Renomear arquivos.py
@@ -32,6 +32,14 @@ pos_initial = 1
 path = path_user if path_user != '' else "./"
 
 filename_list = os.listdir(path)
+length_filename_list = len(list(str(len(filename_list))))
+
+def set_number(pos_initial, digits):
+    number = '0'*digits + str(pos_initial)
+    
+    number_limit = eval('1' + '0'*(digits-1))
+    
+    return number[digits-1:] if pos_initial < number_limit else number[digits:]
 
 def filter_filename(filename):
     filename_reverse = filename[::-1]
@@ -39,15 +47,14 @@ def filter_filename(filename):
     filename_pure = filename_reverse[dot_position+1:][::-1]
     if '.' in filename_pure[:5]:
         filename = filename.replace('feat.', 'feat')
-        filename_reverse = filename[::-1]
-        dot_position = filename_reverse.index('.')
-        filename_pure = filename_pure[dot_position:]
+        dot_position = filename.index('.')
+        filename_pure = filename_pure[dot_position+1:]
     return filename_pure
 
-def rename_file(filename, pos_initial, ext_user):
-    global filename_list
+def rename_file(filename, pos_initial, ext_user, length_filename_list):
+    global filename_list, path
     
-    number = '0' + str(pos_initial) if pos_initial < 10 else str(pos_initial)
+    number = set_number(pos_initial, length_filename_list)
     filename_filtered = filter_filename(filename)
     
     new_filename = number + ' - ' + filename_filtered + ext_user
@@ -69,9 +76,9 @@ if extension_file_user == "":
     for filename in tqdm(filename_list):
         extension_file_user = get_file_extension(filename)
         if extension_file_user != '.py':
-            rename_file(filename, pos_initial, extension_file_user)
+            rename_file(filename, pos_initial, extension_file_user, length_filename_list)
             pos_initial += 1
 else:
     for filename in tqdm(filename_list):
-        rename_file(filename, pos_initial, extension_file_user)
+        rename_file(filename, pos_initial, extension_file_user, length_filename_list)
         pos_initial += 1
