ca0d0404121ac45747d01260cc8f7ad50531983f
vcsil <vcsilva@poli.ufrj.br>
2023-04-29 16:39:53 -0300

    :ok_hand: style: eslint py

--- Renomear arquivos.py
+++ Renomear arquivos.py
@@ -19,11 +19,13 @@ except ValueError:
 
 shuffle = bool(int(input("Shuffle (1) or no shuffle (0)?: ")))
 
+
 def get_file_extension(filename):
     filename_reverse = filename[::-1]
     dot_position = filename_reverse.index('.')
     ext = filename_reverse[:dot_position][::-1]
     return '.' + ext
+
 
 def get_first_extension(directory):
     first_arq = os.listdir(directory)[0]
@@ -36,13 +38,15 @@ path = path_user if path_user != '' else "./"
 filename_list = os.listdir(path)
 length_filename_list = len(list(str(len(filename_list))))
 
+
 def set_number(pos_initial, digits):
     number = '0'*digits + str(pos_initial)
-    
+
     number_limit = eval('1' + '0'*(digits-1))
     number = '0'+number if pos_initial < 10 else number
-    
+
     return number[digits-1:] if pos_initial < number_limit else number[digits:]
+
 
 def filter_filename(filename):
     filename_reverse = filename[::-1]
@@ -57,26 +61,28 @@ def filter_filename(filename):
         filename_pure = filename_pure[trace_position+3:]
     return filename_pure
 
+
 def rename_file(filename, pos_initial, ext_user, length_filename_list):
     global filename_list, path
-    
+
     number = set_number(pos_initial, length_filename_list)
     filename_filtered = filter_filename(filename)
-    
+
     new_filename = number + ' - ' + filename_filtered + ext_user
-    
+
     try:
-            os.rename(path + '/' + filename, path + '/' + new_filename )
-            print('\n' + new_filename)
-            return 
+        os.rename(path + '/' + filename, path + '/' + new_filename)
+        print('\n' + new_filename)
+        return
     except:
         pos_initial += 1
         print('=-=-'*10)
         rename_file(filename, pos_initial, ext_user)
 
-    #print("arquivo " + filename + " alterado para " + new_filename)
-    #print('\n' + filename)
-    #print(new_filename)
+    # print("arquivo " + filename + " alterado para " + new_filename)
+    # print('\n' + filename)
+    # print(new_filename)
+
 
 if (shuffle):
     random.shuffle(filename_list)
